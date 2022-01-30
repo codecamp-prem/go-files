ad221f438f99818310b11733b7392f92d4f6d75a
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-01-30 08:26:27 +0545

    Added maps functionality of Golang in readFiles/count/main.go

---
+++ readFiles/count/main.go
@@ -0,0 +1,24 @@
+// count tallies the number of times each line
+// occurs within a file
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/codecamp-prem/readFiles/datafile"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counts := make(map[string]int)
+	for _, line := range lines {
+		counts[line]++
+	}
+	for name, count := range counts {
+		fmt.Printf("Votes for %s: %d\n", name, count)
+	}
+}
