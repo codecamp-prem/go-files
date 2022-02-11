5d2621db18ae3b8b8586e99085c005e86a6c17a6
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-02-11 15:58:47 +0545

    Using type Assertion in interface/starlordThings/main.go - Added type assertion to get a value of a concrete type back

--- interface/starlordThing/main.go
+++ interface/starlordThing/main.go
@@ -4,9 +4,21 @@ import (
 	"github.com/codecamp-prem/interface/starlordThing/gadget"
 )
 
+// Player : interface
 type Player interface {
 	Play(string)
 	Stop()
+}
+
+// Tryout : When you have a value of a concrete type assigned to a variable with an interface type, a type assertion lets you get the concrete type back. It’s kind of like a type conversion. Its syntax even looks like a cross between a method call and a type conversion. After an interface value, you type a dot,followed by a pair of parentheses with the concrete type. */
+// type assertion to get a value of a concrete type back,
+func Tryout(player Player) {
+	player.Play("Sexy Back")
+	player.Stop()
+	recorder, ok := player.(gadget.TapeRecorder)
+	if ok {
+		recorder.Record()
+	}
 }
 
 func playList(device Player, songs []string) {
@@ -18,8 +30,11 @@ func playList(device Player, songs []string) {
 
 func main() {
 	songsList := []string{"Jai Ho", "Rangeela marooo", "Ringadridham"}
-	var player Player = gadget.TapePlayer{}
+	var player Player = gadget.TapePlayer{} // {} for struct defined type
 	playList(player, songsList)
 	player = gadget.TapeRecorder{}
 	playList(player, songsList)
+
+	Tryout(gadget.TapePlayer{})
+	Tryout(gadget.TapeRecorder{})
 }
