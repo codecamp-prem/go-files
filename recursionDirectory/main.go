bd100459b83ea47d091527f8991062c88e790a12
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-02-17 18:03:26 +0545

    In recursionDirectory - Add error msg if error occur while scaning permission issue directory

--- recursionDirectory/main.go
+++ recursionDirectory/main.go
@@ -11,6 +11,7 @@ func scanDirectory(path string) error {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path) // Get the slice with directory content
 	if err != nil {
+		fmt.Printf("Returning error from scanDirectory(\"%s\") call\n", path)
 		return err
 	}
 	for _, file := range files {
@@ -18,6 +19,7 @@ func scanDirectory(path string) error {
 		if file.IsDir() {                            // if this is a subdirectory
 			err := scanDirectory(filePath) // recursively call scanDirectory, this time with the subdirectory path
 			if err != nil {
+				fmt.Printf("Returning error from scanDirectory(\"%s\") call", path)
 				return err
 			}
 		} else {
